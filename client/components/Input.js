c9df0cb3db04d35c4812f59c4370847ea321559d
augusto-proano <tito4gsus@gmail.com>
2019-03-12 15:40:05 -0400

    (Add Feat): ability to fetch forecast to the front-end

--- client/components/Input.js
+++ client/components/Input.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Input = () => {
-    return (
-        <div>
-            <h1>Weather 7 Days Forecast</h1>
-            <h2>Input your city or zip code</h2>
-            <input />
-        </div>
-    )
+  //State Hooks
+  const [location, setLocation] = useState('')
+  const [forecast, setForecast] = useState({})
+
+  const handleLocation = e => {
+    const { value } = e.target
+    setLocation(value)
+  }
+  const fetchForecast = async () => {
+    try {
+      const res = await axios.post('/api/forecast', { location })
+    setForecast(res.data)
+    console.log('RES', res.data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  return (
+    <div>
+      <h1>Weather 7 Days Forecast</h1>
+      <h2>Input your city or zip code</h2>
+      <input onChange={handleLocation} />
+      <div>
+        <h2>Token:</h2>
+        <input />
+        <button>Free Token</button>
+      </div>
+      <button onClick={fetchForecast}>Forecast</button>
+    </div>
+  )
 }
 
 export default Input
--- server/api/forecast.js
+++ server/api/forecast.js
@@ -4,16 +4,21 @@ module.exports = router
 
 const currentToken = 'helloworld'
 const weatherApi = request.createClient(
-    'https://www.metaweather.com/api/location/'
-    )
+  'https://www.metaweather.com/api/location/'
+)
 
 router.post('/', async (req, res, next) => {
   const { location, token } = req.body
-  //Get where on Earth ,ID
-  const city = await weatherApi.get(`search/?query=${location}`)
-  const { woeid } = city.body[0]
 
-  //Get and send forecast from API
-  const forecast = await weatherApi.get(`${woeid}`)
-  res.send(forecast.body)
+  try {
+    //Get where on Earth ,ID
+    const city = await weatherApi.get(`search/?query=${location}`)
+    const { woeid } = city.body[0]
+
+    //Get and send forecast from API
+    const forecast = await weatherApi.get(`${woeid}`)
+    res.send(forecast.body)
+  } catch (err) {
+    res.status(404).send('Location Not Found')
+  }
 })
