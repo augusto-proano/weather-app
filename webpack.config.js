a74acf47951027c45f9eeb1451ea5b6434107261
augusto-proano <tito4gsus@gmail.com>
2019-03-12 20:57:25 -0400

    (Setup): sass is compiled through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
+const Fiber = require('fiber')
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
@@ -20,6 +21,20 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+            options: {
+              implementation: require('sass'),
+              fiber: Fiber
+            }
+          }
+        ]
       }
     ]
   }
